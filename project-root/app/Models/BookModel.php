62aa53746358309f539fddabe72499c7b2474186
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2026-04-05 16:41:38 -0600

    feat: Tutorial de API REST

---
+++ project-root/app/Models/BookModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BookModel extends Model
+{
+    protected $table = 'books';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['title', 'author_id', 'year'];
+    protected $useTimeStamp = true;
+
+    public function withAuthorInfo()
+    {
+        return $this
+            ->select('books.*, authors.name as author_name')
+            ->join('authors', 'books.author_id = authors.id');
+    }
+
+}
---
+++ project-root/app/Transformers/BookTransformer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Transformers;
+
+use CodeIgniter\API\BaseTransformer;
+
+class BookTransformer extends BaseTransformer
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param mixed $resource
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(mixed $resource): array
+    {
+        return [
+            'id'    => $resource['id'],
+            'title' => $resource['title'],
+            'year'  => $resource['year'],
+        ];
+    }
+
+    protected function includeAuthor(array $book): ?array
+    {
+        if (empty($book['author_id']) || empty($book['author_name'])) {
+            return null;
+        }
+
+        return [
+            'id'   => $book['author_id'],
+            'name' => $book['author_name'],
+        ];
+    }
+}
